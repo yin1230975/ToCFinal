085cbf8fd92b97df2e683186a4283433b626430f
Wiily Yin <willy19981031@gmail.com>
2022-12-10 01:52:35 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+
+import os
+import random
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import configparser
+
+
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+
+        return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.source.user_id =='Udeadbeefdeadbeefdeadbeefdeadbeef': #填入line在verify時回傳的user_id
+        return 'OK'
+    get_message = event.message.text
+
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
+
+if __name__ == "__main__":
+    app.run(port=5002)
